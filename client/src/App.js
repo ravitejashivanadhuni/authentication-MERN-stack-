b5c27319d7fb8305d33839f677413e741d52b1ed
ravitejashivanadhuni <ravitejashivanadhuni@gmail.com>
2026-01-05 01:12:43 +0530

    added dashboard.jsx

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Landing from "./pages/landing";
 import ForgotPassword from "./pages/forgot";
+import Dashboard from "./pages/dashboard";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/" element={<Landing />} />
           <Route path="/forgot-password-page" element={<ForgotPassword />} />
+          <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
       </Router>
     </ThemeProvider>
