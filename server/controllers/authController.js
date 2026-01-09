183777c55c85ac1423df4cfb016054d9d8825958
ravitejashivanadhuni <ravitejashivanadhuni@gmail.com>
2026-01-09 22:11:29 +0530

    updated authcontroller.jsx

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -129,7 +129,7 @@ exports.login = async (req, res) => {
 
     const isMatch = await user.comparePassword(password);
     if (!isMatch) {
-      return res.status(400).json({ success: false, message: 'Invalid credentials' });
+      return res.status(400).json({ success: false, message: 'Incorrect Password!' });
     }
 
     if (!user.isVerified) {
@@ -141,7 +141,7 @@ exports.login = async (req, res) => {
       process.env.JWT_SECRET,
       { expiresIn: '1d' }
     );
-
+    console.log('Generated JWT Token:', token);
     res.status(200).json({
       success: true,
       message: 'Login successful',
